0717fb02c0fd8431e659086963d7f020ee92b530
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-29 09:38:45 -0500

    update-home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import {Switch,Route} from 'react-router-dom'
+import Home from './pages/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       
+      <Switch>
+        <Route exact path="/">
+         <Home/>
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+       <div className='header-home'>header</div>
+       <div  className='main'>Main</div>
+      {/* 
+       <div>footer</div>  */}
+    </div>
+  );
+}
+
+export default Home;
