f75fabe7ed74a1845d1425bc664837f03a458528
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-29 10:34:47 -0500

    added-footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='footer-home'>footer</div> 
+    <div className='footer-home'>
+        <div className='box'>1</div>
+        <div className='box'>2</div>
+        <div className='box'>3</div>
+       
+    </div> 
   );
 }
 
