43a99e18e9f5e06f71ff3711c7e5b6375ea62644
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-29 09:45:45 -0500

    added-main

---
+++ src/component/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div  className='main'>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi asperiores recusandae excepturi non, sapiente autem! Atque, similique, ducimus quod ipsum nobis nisi unde eos voluptatem iusto quae libero sequi fugit debitis eveniet animi dolores vero optio eius consequuntur. Perferendis, sed impedit ipsa fuga, dolores exercitationem at, unde aliquam dicta magnam quis inventore provident atque! Id quos odit tempora similique quam eveniet, eaque temporibus ut repellat asperiores beatae officiis dolore facilis molestias accusamus porro excepturi velit natus. Reprehenderit, sunt. Cum officiis nostrum temporibus dolorem, vitae veniam quisquam modi eveniet
+     incidunt tenetur officia est minus? Quasi ut sit eligendi eos 
+     corrupti! Natus.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi asperiores recusandae excepturi non, sapiente autem! Atque, similique, ducimus quod ipsum nobis nisi unde eos voluptatem iusto quae libero sequi fugit debitis eveniet animi dolores vero optio eius consequuntur. Perferendis, sed impedit ipsa fuga, dolores exercitationem at, unde aliquam dicta magnam quis inventore provident atque! Id quos odit tempora similique quam eveniet, eaque temporibus ut repellat asperiores beatae officiis dolore facilis molestias accusamus porro excepturi velit natus. Reprehenderit, sunt. Cum officiis nostrum temporibus dolorem, vitae veniam quisquam modi eveniet
+     incidunt tenetur officia est minus? Quasi ut sit eligendi eos 
+     corrupti! Natus.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi asperiores recusandae excepturi non, sapiente autem! Atque, similique, ducimus quod ipsum nobis nisi unde eos voluptatem iusto quae libero sequi fugit debitis eveniet animi dolores vero optio eius consequuntur. Perferendis, sed impedit ipsa fuga, dolores exercitationem at, unde aliquam dicta magnam quis inventore provident atque! Id quos odit tempora similique quam eveniet, eaque temporibus ut repellat asperiores beatae officiis dolore facilis molestias accusamus porro excepturi velit natus. Reprehenderit, sunt. Cum officiis nostrum temporibus dolorem, vitae veniam quisquam modi eveniet
+     incidunt tenetur officia est minus? Quasi ut sit eligendi eos 
+     corrupti! Natus.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi asperiores recusandae excepturi non, sapiente autem! Atque, similique, ducimus quod ipsum nobis nisi unde eos voluptatem iusto quae libero sequi fugit debitis eveniet animi dolores vero optio eius consequuntur. Perferendis, sed impedit ipsa fuga, dolores exercitationem at, unde aliquam dicta magnam quis inventore provident atque! Id quos odit tempora similique quam eveniet, eaque temporibus ut repellat asperiores beatae officiis dolore facilis molestias accusamus porro excepturi velit natus. Reprehenderit, sunt. Cum officiis nostrum temporibus dolorem, vitae veniam quisquam modi eveniet
+     incidunt tenetur officia est minus? Quasi ut sit eligendi eos 
+     corrupti! Natus.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi asperiores recusandae excepturi non, sapiente autem! Atque, similique, ducimus quod ipsum nobis nisi unde eos voluptatem iusto quae libero sequi fugit debitis eveniet animi dolores vero optio eius consequuntur. Perferendis, sed impedit ipsa fuga, dolores exercitationem at, unde aliquam dicta magnam quis inventore provident atque! Id quos odit tempora similique quam eveniet, eaque temporibus ut repellat asperiores beatae officiis dolore facilis molestias accusamus porro excepturi velit natus. Reprehenderit, sunt. Cum officiis nostrum temporibus dolorem, vitae veniam quisquam modi eveniet
+     incidunt tenetur officia est minus? Quasi ut sit eligendi eos 
+     corrupti! Natus.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi asperiores recusandae excepturi non, sapiente autem! Atque, similique, ducimus quod ipsum nobis nisi unde eos voluptatem iusto quae libero sequi fugit debitis eveniet animi dolores vero optio eius consequuntur. Perferendis, sed impedit ipsa fuga, dolores exercitationem at, unde aliquam dicta magnam quis inventore provident atque! Id quos odit tempora similique quam eveniet, eaque temporibus ut repellat asperiores beatae officiis dolore facilis molestias accusamus porro excepturi velit natus. Reprehenderit, sunt. Cum officiis nostrum temporibus dolorem, vitae veniam quisquam modi eveniet
+     incidunt tenetur officia est minus? Quasi ut sit eligendi eos 
+     corrupti! Natus.
+    </div>
+  );
+}
+
+export default Main;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Main from '../component/Main'
 const Home = () => {
   return (
     <div className="home">
        <div className='header-home'>header</div>
-       <div  className='main'>Main</div>
+         <Main/>
       {/* 
        <div>footer</div>  */}
     </div>
